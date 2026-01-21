633a6c1bc692d2356c23e6d5c8d67c04324a4b74
Sarath Srinivasan <2023eeb1242@iitrpr.ac.in>
2026-01-21 19:28:23 +0530

    Update count_package.sv

--- count_package.sv
+++ count_package.sv
@@ -2,14 +2,14 @@ package count_pkg;
   
   int no_of_transactions = 1;
   
-  include "count_trans.sv" 
-  include "count_gen.sv"
-  include "driver.sv"
-  include "write_mon.sv" 
-  include "read_mon.sv"
-  include "count_model.sv"
-  include "count_score.sv"
-  include "count_env.sv" 
-  include "count_test.sv"
+  `include "count_trans.sv" 
+  `include "count_gen.sv"
+  `include "driver.sv"
+  `include "write_mon.sv" 
+  `include "read_mon.sv"
+  `include "count_model.sv"
+  `include "count_score.sv"
+  `include "count_env.sv" 
+  `include "count_test.sv"
   
 endpackage
